1077f2caa38683054d96c5161fe391dfa357d6ff
richard <r.douglas.42092@gmail.com>
2024-06-19 12:39:13 -0400

    minor prompt update. yes html? I think we should just try GPT 4 for now...

--- util/openAI/openAICall.js
+++ util/openAI/openAICall.js
@@ -25,7 +25,7 @@ async function fetchData(sysPrompt, prompt, config){
             "content-Type": "application/json"
         },
         body: JSON.stringify({
-            model:"gpt-3.5-turbo",
+            model:"gpt-4-0613",
             messages: [{role:"user",content:sysPrompt},
                 {role: "user", content: prompt},
                 {role: "user", content: config},
