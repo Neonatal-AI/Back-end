e9776305a338c532a24c9e577f350accef2dd3ce
richard <r.douglas.42092@gmail.com>
2024-09-20 18:19:57 -0400

    return after sending response

--- server.js
+++ server.js
@@ -162,8 +162,8 @@ app.post('/createDocs', async (req, res) => {
                 console.log(promptResponse.choices[0])
                 console.log("****************************************************************")
         return res.status(200).send({
-            document: promptResponse,
-            prompt: prompt
+            document: "promptResponse",
+            prompt: "prompt"
         });
     }catch(error){
         console.log(error)
